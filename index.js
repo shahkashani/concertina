38cb1ba1fdf606d284c714e367fa604b4ab1deb5
Shah Kashani <skashani@gmail.com>
2019-06-01 16:48:52 -0400

    Sessions

--- index.js
+++ index.js
@@ -2,18 +2,58 @@ require('dotenv').config();
 
 const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
-const app = express();
+const session = require('express-session');
 
 const PORT = process.env.PORT || 3000;
 
 const {
   SPOTIFY_REDIRECT_URI,
   SPOTIFY_CLIENT_ID,
-  SPOTIFY_CLIENT_SECRET
+  SPOTIFY_CLIENT_SECRET,
+  EXPRESS_SESSION_SECRET
 } = process.env;
 
 const SCOPES = ['user-read-currently-playing', 'user-read-recently-played'];
 const STATE = 'concertina';
+
+const app = express();
+
+app.use(
+  session({
+    secret: EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  })
+);
+
+const setAuthDataFromResponse = (req, spotifyApi, auth) => {
+  const { access_token: accessToken, refresh_token: refreshToken } = auth.body;
+  if (accessToken) {
+    spotifyApi.setAccessToken(accessToken);
+  }
+  if (refreshToken) {
+    spotifyApi.setRefreshToken(refreshToken);
+  }
+  const previousSession = getAuthData(req);
+  req.session.spotifySession = {
+    accessToken: accessToken || previousSession.accessToken,
+    refreshToken: refreshToken || previousSession.refreshToken
+  };
+};
+
+const getAuthData = req => {
+  const { spotifySession } = req.session;
+  return spotifySession || {};
+};
+
+// Just a handy wrapper that also refreshes the auth token
+const makeSpotifyCall = async (req, spotifyApi, method) => {
+  const result = await spotifyApi[method]();
+  const refresh = await spotifyApi.refreshAccessToken();
+  setAuthDataFromResponse(req, spotifyApi, refresh);
+  return result;
+};
 
 app.get('/', async (req, res) => {
   const { code } = req.query;
@@ -24,22 +64,32 @@ app.get('/', async (req, res) => {
     clientSecret: SPOTIFY_CLIENT_SECRET
   });
 
-  if (!code) {
+  const { accessToken, refreshToken } = getAuthData(req);
+
+  if (!code && (!accessToken || !refreshToken)) {
     res.redirect(spotifyApi.createAuthorizeURL(SCOPES, STATE, false));
     return;
   }
 
   try {
-    const auth = await spotifyApi.authorizationCodeGrant(code);
+    if (accessToken && refreshToken) {
+      spotifyApi.setAccessToken(accessToken);
+      spotifyApi.setRefreshToken(refreshToken);
+    } else {
+      const auth = await spotifyApi.authorizationCodeGrant(code);
+      setAuthDataFromResponse(req, spotifyApi, auth);
+      // Get rid of that ?code in the URL
+      res.redirect(req.path);
+      return;
+    }
 
-    spotifyApi.setAccessToken(auth.body['access_token']);
-    spotifyApi.setRefreshToken(auth.body['refresh_token']);
-
-    const tracks = await spotifyApi.getMyRecentlyPlayedTracks({});
-
-    res.send(
-      `Do the thing! <pre>${JSON.stringify(tracks.body, null, 2)}</pre>`
+    const tracks = await makeSpotifyCall(
+      req,
+      spotifyApi,
+      'getMyRecentlyPlayedTracks'
     );
+
+    res.send(`Do the thing! <pre>${JSON.stringify(tracks, null, 2)}</pre>`);
   } catch (err) {
     res.send(`No! ${err}`);
   }
